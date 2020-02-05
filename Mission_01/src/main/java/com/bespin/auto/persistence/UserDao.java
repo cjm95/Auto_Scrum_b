e5f0bdd338359bfa4c90523979a708b04f675a08
sh <seoheee96@naver.com>
2020-02-05 14:42:57 +0900

    서희 insertTest 수정

--- Mission_01/src/main/java/com/bespin/auto/persistence/UserDao.java
+++ Mission_01/src/main/java/com/bespin/auto/persistence/UserDao.java
@@ -2,8 +2,11 @@ package com.bespin.auto.persistence;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.bespin.auto.domain.User;
 
+@Repository
 public interface UserDao {
 	List<User> getDBAll();
 	List<User> getDB(String id, int i);
---
+++ Mission_01/src/test/java/com/bespin/auto/insertTest.java
@@ -0,0 +1,37 @@
+package com.bespin.auto;
+
+import java.sql.Connection;
+
+import javax.activation.DataSource;
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.bespin.auto.domain.User;
+import com.bespin.auto.persistence.UserDao;
+import com.bespin.auto.persistence.UserDaoImple;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+public class insertTest {
+	
+	//@Inject
+	@Autowired
+	private UserDao userdoa;
+	
+	@Test
+	public void insertTest() {
+		User user =new User();
+		user.setName("테스트다");
+		user.setNum(819);
+		user.setAge(26);
+		user.setTeam("CMS Automation!");
+		
+		userdoa.insert(user);
+		
+	}
+}
