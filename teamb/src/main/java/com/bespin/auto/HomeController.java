af1ad902738c7ad37789d60cd00dc443d8eed835
ju0731 <hanju.kim@bespinglobal.com>
2020-02-07 16:34:44 +0900

    jenkins test

--- teamb/src/main/java/com/bespin/auto/HomeController.java
+++ teamb/src/main/java/com/bespin/auto/HomeController.java
@@ -114,8 +114,7 @@ public class HomeController {
 				user.setCurnum(Integer.parseInt(bn[i].replace("BG", "")));
 				int result = userService.delete(user.getCurnum());
 			}
-			return new RedirectView("http://ec2-13-125-253-208.ap-northeast-2.compute.amazonaws.com
-:8088/auto/members");
+			return new RedirectView("http://ec2-13-125-253-208.ap-northeast-2.compute.amazonaws.com:8088/auto/members");
 	}
 
 	
