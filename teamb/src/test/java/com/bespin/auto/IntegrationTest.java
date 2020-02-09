31f15e6bf351a62400bce285daa8839828c1f4df
ju0731 <hanju.kim@bespinglobal.com>
2020-02-10 08:59:34 +0900

    integration test update

--- teamb/src/test/java/com/bespin/auto/IntegrationTest.java
+++ teamb/src/test/java/com/bespin/auto/IntegrationTest.java
@@ -1,55 +1,94 @@
 package com.bespin.auto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.sound.midi.MidiDevice.Info;
+
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bespin.auto.domain.User;
+import com.bespin.auto.persistence.UserDao;
+import com.bespin.auto.persistence.UserDaoImple;
 import com.bespin.auto.service.UserService;
+import com.bespin.auto.service.UserServiceImple;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Transactional
 public class IntegrationTest {
 
 	private ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 	private MockMvc mockMvc;
+	private final Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
+	User user;
 	
 	@Mock
 	private UserService userService;
 	
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+  	private SqlSession sqlSession;
+
 	@InjectMocks
 	private HomeController objectUnderTest;
+	
 	
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		mockMvc = MockMvcBuilders.standaloneSetup(objectUnderTest).build();
+	    user = new User(808,"jj","Auto",26);
+	    sqlSession.insert("bespin.spring.mappers.UserMapper.insert", user);
 	}
 	
 	@Test
 	public void getAllMemberTest() throws Exception {
 		when(userService.getDBAll()).thenReturn((java.util.List<User>)new ArrayList<User>());
 		
-		mockMvc.perform(get("/members").accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+		mockMvc.perform(get("/members"))
 		.andDo(print())
 		.andExpect(status().is2xxSuccessful());
+		
+        List<User> users = userDao.getDBAll();
+        logger.info("users : {}", users);
 	}
 	
     @Test     
@@ -58,6 +97,9 @@ public class IntegrationTest {
    			.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
 			.andDo(print())
 			.andExpect(status().is2xxSuccessful());
+   	 
+     List<User> users = userDao.getDB("808",0);
+     logger.info("users : {}", users);
    	 } 
 	
 	@Test
@@ -76,15 +118,19 @@ public class IntegrationTest {
 	@Test
 	public void updatePutMember() throws Exception {
 		
-		mockMvc.perform(put("/modification/242")
+		mockMvc.perform(put("/modification/808")
 				.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-   			 .param("usernum","111")
+   			 .param("usernum","808")
 		    	 .param("username","jd")
 				.param("department","team")
 				.param("age","20"))
 		.andDo(print())
 		.andExpect(redirectedUrl("/members"));
 		
+		user.setName("jd");
+		user.setCurnum(808);
+	     int users = userDao.update(808,user);
+	     logger.info("count : {}", users);
 	}
 	
 	@Test
@@ -94,6 +140,9 @@ public class IntegrationTest {
 		.andDo(print())
 		.andExpect(redirectedUrl("http://ec2-13-125-253-208.ap-northeast-2.compute.amazonaws.com:8088/auto/members"));
 		
+		user.setNum(808);
+	     int users = userDao.delete(808);
+	     logger.info("count : {}", users);
 	}
 	
 
